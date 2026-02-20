6bc37b1c8f22c77720a8944bbc75781b2a98ebd5
Christian Johnson <ogf10@txstate.edu>
2026-02-20 14:40:10 -0600

    Added SearchButton function and applied it to the webapp

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -9,6 +9,18 @@ function SearchForm() {
     <div className={styles.searchForm}>
       <p>SearchForm placeholder</p>
     </div>
+      <div className={styles.searchForm}>
+        <p> Press Search for recipes with your ingredients</p>
+        <SearchButton />
+        <p>{SearchButton.result}</p>
+      </div>
+
+function SearchButton() {
+  function handleClick() {
+    result = fetch('/themealdb.com/api/json/v1/1/filter.php?i=' + 'chicken')
+  }
+  return (
+    <button onClick={handleClick}>Search</button>
   )
 }
 
