5bc1356d1b3915730231a994dc3a28360bb9e0ae
Tina Carter <lgw71@txstate.edu>
2026-02-27 13:14:47 -0600

    Don't print if missing fields
    if Title/Author/Ingredients/Instructions are not formatted, do not print.
    
    >in future potentially change to warning with user override to 'print anyway'?

--- src/components/DownloadButton/DownloadButton.jsx
+++ src/components/DownloadButton/DownloadButton.jsx
@@ -6,7 +6,19 @@ import styles from './DownloadButton.module.css'
 
 function DownloadButton() {
   return (
-    <button className={styles.downloadButton} onClick={() => print()}>
+    <button className={styles.downloadButton} onClick={() => {
+      if(document.body.classList.contains('recipeTitle') && 
+      document.body.classList.contains('recipeAuthor') && 
+      document.body.classList.contains('ingredientsList') && 
+      document.body.classList.contains('recipeInstructions')) {
+        // Open the print dialog for the current page
+        window.print();
+      }
+      else {
+        // If the required elements are not present, show an alert
+      }
+      window.alert('This Recipe is missing some or all of its information and cannot be printed.');
+    }}>
       Download / Print
     </button>
   )
