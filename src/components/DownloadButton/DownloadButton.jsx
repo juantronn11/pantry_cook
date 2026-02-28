52d34b28f37d88ea000af549d2595a45266f6163
Tina Carter <lgw71@txstate.edu>
2026-02-27 18:36:50 -0600

    remove requirement for author

--- src/components/DownloadButton/DownloadButton.jsx
+++ src/components/DownloadButton/DownloadButton.jsx
@@ -8,7 +8,6 @@ function DownloadButton() {
   return (
     <button className={styles.downloadButton} onClick={() => {
       if(document.body.classList.contains('recipeTitle') && 
-      document.body.classList.contains('recipeAuthor') && 
       document.body.classList.contains('ingredientsList') && 
       document.body.classList.contains('recipeInstructions')) {
         // Open the print dialog for the current page
@@ -16,6 +15,7 @@ function DownloadButton() {
       }
       else {
         // If the required elements are not present, show an alert
+        window.alert('This Recipe is missing some or all of its information and cannot be downloaded or printed.');
       }
       window.alert('This Recipe is missing some or all of its information and cannot be printed.');
     }}>
