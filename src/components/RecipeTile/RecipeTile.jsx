b585f6bb2535c452d0f9f25b6440d3369afe36fd
Tina Carter <lgw71@txstate.edu>
2026-03-10 13:50:40 -0500

    SCRUM-74: Implement error handling for image loading
    > RecipeTile.jsx
    - useState() imageError
    - handleImageError //change state and allert user
    - change image loading in return() to implement error handling

--- src/components/RecipeTile/RecipeTile.jsx
+++ src/components/RecipeTile/RecipeTile.jsx
@@ -18,6 +18,7 @@ function RecipeTile({recipe}) {
   const [modalData, setModalData] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
+  const [imageError, setImageError] = useState(false);
 
   // SCRUM-71: Transform Spoonacular's recipe format into MealDB's format
   // so the modal rendering code works the same for both API sources.
@@ -75,6 +76,12 @@ function RecipeTile({recipe}) {
     setError(false);
   };
 
+  //SCRUM-74: Error handling for missing/invalid images. If the image fails to load, hide default broken image icon.
+  const handleImageError = () => {
+    setImageError(true);
+  };
+  
+
   return (
 
     <>
@@ -103,7 +110,7 @@ function RecipeTile({recipe}) {
             ) : (
               <>
                   <DownloadButton></DownloadButton>
-                  <img src={modalData.strMealThumb} alt={modalData.strMeal} className={styles.modalImg} />
+                  <img src={imageError ? '../../../media/chicken_alfredo.jpg' : modalData.strMealThumb} alt={imageError ? 'Error' : modalData.strMeal} className={imageError ? styles.error : styles.modalImg} onError={handleImageError} />
                   <h2 className={styles.recipeTitle}>{modalData.strMeal}</h2>
                   <p className={styles.other}><strong>Category:</strong> {modalData.strCategory}</p>
                   <p className={styles.other}><strong>Area:</strong> {modalData.strArea}</p>
