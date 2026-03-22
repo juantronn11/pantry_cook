c474416815ee56b960bd9ae8251988f639c3e6c5
Miguel Alvarez <pqg5@txstate.edu>
2026-03-21 19:12:28 -0500

    SCRUM-92 Login/out Button created
    Log in and log out as well as signup button are functioning. No CSS on buttons but work as planned. Placed in NAVBAR for the time being but can be changed with further discussion.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -10,6 +10,7 @@
 import { useState } from 'react'
 import { NavLink, useNavigate } from 'react-router-dom'
 import { useRecipeContext } from '../../context/RecipeContext'
+import LoginButton from '../AccessButtons/LoginButton'
 import styles from './Navbar.module.css'
 
 // Navbar renders the top navigation bar on every page.
@@ -43,6 +44,7 @@ function Navbar() {
       <NavLink to="/" className={styles.logo} title="Go to home page">
         Pantry Cook
       </NavLink>
+      <LoginButton/>
 
       {/* Hamburger button — hidden on desktop, shown on mobile.
           Toggles menuOpen between true and false on each click */}
